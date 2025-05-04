7366c3b13fee92e2ec5c5087e8f14d10823623d5
fadlyalvi <muhamadfadlyalviansyah@smkwikrama.sch.id>
2025-05-04 15:19:40 +0700

    Revert "feat: add User model and migration"
    This reverts commit 13a2658d3acb1c17e68c408883be7da868566a72.

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,29 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'age',
+        'password',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
